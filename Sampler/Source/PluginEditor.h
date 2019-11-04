74a959d610a88e19763d13b44ccb8a6e688e9944
juan chozas <juanchozass@gmail.com>
2019-11-04 16:25:22 +0100

    remove editors cpp

--- Sampler/Source/PluginEditor.h
+++ Sampler/Source/PluginEditor.h
@@ -19,12 +19,33 @@
 class SamplerAudioProcessorEditor  : public AudioProcessorEditor
 {
 public:
-    SamplerAudioProcessorEditor (SamplerAudioProcessor&);
-    ~SamplerAudioProcessorEditor();
+    SamplerAudioProcessorEditor (SamplerAudioProcessor&) : AudioProcessorEditor (&p), processor (p)
+    {
+        // Make sure that before the constructor has finished, you've set the
+        // editor's size to whatever you need it to be.
+        setSize (400, 300);
+    }
+
+    ~SamplerAudioProcessorEditor()
+    {
+    }
 
     //==============================================================================
-    void paint (Graphics&) override;
-    void resized() override;
+    void paint (Graphics&) override
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+        g.setColour (Colours::white);
+        g.setFont (15.0f);
+        g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+    }
+
+    void resized() override
+    {
+        // This is generally where you'll want to lay out the positions of any
+        // subcomponents in your editor..
+    }
 
 private:
     // This reference is provided as a quick way for your editor to

e1a98592ef08d8a859880a55821cb8f3319ee3e7
juan chozas <juanchozass@gmail.com>
2019-11-06 18:57:11 +0100

    working thumbnail

--- Sampler/Source/PluginEditor.h
+++ Sampler/Source/PluginEditor.h
@@ -12,40 +12,91 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "PluginProcessor.h"
+#include "SamplerThumbnail.h"
 
 //==============================================================================
 /**
 */
-class SamplerAudioProcessorEditor  : public AudioProcessorEditor
+class SamplerAudioProcessorEditor:
+    public AudioProcessorEditor,
+    private ChangeListener
 {
 public:
-    SamplerAudioProcessorEditor (SamplerAudioProcessor&) : AudioProcessorEditor (&p), processor (p)
+    SamplerAudioProcessorEditor (SamplerAudioProcessor& p) : AudioProcessorEditor (&p), processor (p)
     {
-        // Make sure that before the constructor has finished, you've set the
-        // editor's size to whatever you need it to be.
-        setSize (400, 300);
+        addAndMakeVisible (zoomLabel);
+        zoomLabel.setFont (Font (15.00f, Font::plain));
+        zoomLabel.setJustificationType (Justification::centredRight);
+        zoomLabel.setEditable (false, false, false);
+        zoomLabel.setColour (TextEditor::textColourId, Colours::black);
+        zoomLabel.setColour (TextEditor::backgroundColourId, Colour (0x00000000));
+
+        addAndMakeVisible (followTransportButton);
+        followTransportButton.onClick = [this] { updateFollowTransportState(); };
+        followTransportButton.setEnabled (false);
+
+        // TODO ADD FILE LOADING COMPONENT
+
+        addAndMakeVisible (zoomSlider);
+        zoomSlider.setRange (0, 1, 0);
+        zoomSlider.onValueChange = [this] { thumbnail->setZoomFactor (zoomSlider.getValue()); };
+        zoomSlider.setEnabled (false);
+        zoomSlider.setSkewFactor (2);
+
+        thumbnail.reset (new SamplerThumbnail (formatManager, transportSource, zoomSlider));
+        addAndMakeVisible (thumbnail.get());
+        thumbnail->addChangeListener (this);
+
+        addAndMakeVisible (startStopButton);
+        startStopButton.setColour (TextButton::buttonColourId, Colour (0xff79ed7f));
+        startStopButton.setColour (TextButton::textColourOffId, Colours::black);
+        startStopButton.onClick = [this] { startOrStop(); };
+
+        // audio setup
+        formatManager.registerBasicFormats();
+
+        thread.startThread (3);
+        audioDeviceManager.addAudioCallback (&audioSourcePlayer);
+        audioSourcePlayer.setSource (&transportSource);
+
+        setOpaque (true);
+        setSize (1024, 576);
     }
 
     ~SamplerAudioProcessorEditor()
     {
+        transportSource.setSource (nullptr);
+        audioSourcePlayer.setSource (nullptr);
+
+        audioDeviceManager.removeAudioCallback (&audioSourcePlayer);
+
+        thumbnail->removeChangeListener (this);
     }
 
     //==============================================================================
-    void paint (Graphics&) override
+    void paint (Graphics& g) override
     {
-        // (Our component is opaque, so we must completely fill the background with a solid colour)
-        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-        g.setColour (Colours::white);
-        g.setFont (15.0f);
-        g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+        g.fillAll (Colour::fromString("FF292C36"));
     }
 
     void resized() override
     {
-        // This is generally where you'll want to lay out the positions of any
-        // subcomponents in your editor..
+        auto r = getLocalBounds().reduced (4);
+        auto controls = r.removeFromBottom (90);
+        auto controlRightBounds = controls.removeFromRight (controls.getWidth() / 3);
+        auto zoom = controls.removeFromTop (25);
+
+        zoomLabel.setBounds (zoom.removeFromLeft (50));
+        zoomSlider.setBounds (zoom);
+
+        followTransportButton.setBounds (controls.removeFromTop (25));
+        startStopButton.setBounds (controls);
+
+        r.removeFromBottom (6);
+        thumbnail->setBounds (r.removeFromBottom (140));
+        r.removeFromBottom (6);
     }
+
 
 
     //==============================================================================
@@ -56,8 +107,86 @@ private:
     // access the processor object that created it.
     SamplerAudioProcessor& processor;
 
+    AudioDeviceManager audioDeviceManager;
+    AudioFormatManager formatManager;
+    TimeSliceThread thread { "audio file preview" };
+
+    URL currentAudioFile;
+    AudioSourcePlayer audioSourcePlayer;
+    AudioTransportSource transportSource;
+    std::unique_ptr<AudioFormatReaderSource> currentAudioFileSource;
+
+    std::unique_ptr<SamplerThumbnail> thumbnail;
+    Label zoomLabel { {}, "Zoom" };
+    Slider zoomSlider { Slider::LinearHorizontal, Slider::NoTextBox };
+    ToggleButton followTransportButton { "Follow Transport" };
+    TextButton startStopButton { "Play/Stop" };
 
     //==============================================================================
+
+    void showAudioResource (URL resource)
+    {
+        if (loadURLIntoTransport (resource))
+        {
+            currentAudioFile = std::move (resource);
+            zoomSlider.setEnabled (true);
+            followTransportButton.setEnabled (true);
+        }
+
+        zoomSlider.setValue (0, dontSendNotification);
+        thumbnail->setURL (currentAudioFile); // TODO method that prompts the user for a wav file and loads it
+    }
+
+    bool loadURLIntoTransport (const URL& audioURL)
+    {
+        // unload the previous file source and delete it..
+        transportSource.stop();
+        transportSource.setSource (nullptr);
+        currentAudioFileSource.reset();
+
+        AudioFormatReader* reader = nullptr;
+
+        if (reader == nullptr)
+            reader = formatManager.createReaderFor (audioURL.createInputStream (false));
+
+        if (reader != nullptr)
+        {
+            currentAudioFileSource.reset (new AudioFormatReaderSource (reader, true));
+
+            // ..and plug it into our transport source
+            transportSource.setSource (currentAudioFileSource.get(),
+                                       32768,                   // tells it to buffer this many samples ahead
+                                       &thread,                 // this is the background thread to use for reading-ahead TODO necessary?
+                                       reader->sampleRate);     // allows for sample rate correction
+
+            return true;
+        }
+
+        return false;
+    }
+
+    void startOrStop()
+    {
+        if (transportSource.isPlaying())
+        {
+            transportSource.stop();
+        } else
+        {
+            transportSource.setPosition (0);
+            transportSource.start();
+        }
+    }
+
+    void updateFollowTransportState()
+    {
+        thumbnail->setFollowsTransport (followTransportButton.getToggleState());
+    }
+
+    void changeListenerCallback (ChangeBroadcaster* source) override
+    {
+        if (source == thumbnail.get())
+            showAudioResource (URL (thumbnail->getLastDroppedFile()));
+    }
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SamplerAudioProcessorEditor)
 };
--- Sampler/Source/SamplerThumbnail.h
+++ Sampler/Source/SamplerThumbnail.h
@@ -15,47 +15,211 @@
 //==============================================================================
 /*
 */
-class SamplerThumbnail    : public Component
+class SamplerThumbnail: 
+    public Component, 
+    public ChangeListener,
+    public ChangeBroadcaster,
+    private ScrollBar::Listener,
+    private Timer
 {
 public:
-    SamplerThumbnail()
+    SamplerThumbnail(AudioFormatManager& formatManager,
+                     AudioTransportSource& source,
+                     Slider& slider): 
+        transportSource (source),
+        zoomSlider (slider),
+        thumbnail (512, formatManager, thumbnailCache)
     {
-        // In your constructor, you should add any child components, and
-        // initialise any special settings that your component needs.
+        thumbnail.addChangeListener (this);
 
+        addAndMakeVisible (scrollbar);
+        scrollbar.setRangeLimits (visibleRange);
+        scrollbar.setAutoHide (false);
+        scrollbar.addListener (this);
+
+        currentPositionMarker.setFill (Colours::white.withAlpha (0.85f));
+        addAndMakeVisible (currentPositionMarker);
     }
 
     ~SamplerThumbnail()
     {
+        scrollbar.removeListener (this);
+        thumbnail.removeChangeListener (this);
+    }
+
+    void setURL (const URL& url)
+    {
+        InputSource* inputSource = nullptr;
+
+    #if ! JUCE_IOS
+        if (url.isLocalFile())
+        {
+            inputSource = new FileInputSource (url.getLocalFile());
+        } else
+        #endif
+        {
+            if (inputSource == nullptr)
+                inputSource = new URLInputSource (url);
+        }
+
+        if (inputSource != nullptr)
+        {
+            thumbnail.setSource (inputSource);
+
+            Range<double> newRange (0.0, thumbnail.getTotalLength());
+            scrollbar.setRangeLimits (newRange);
+            setRange (newRange);
+
+            startTimerHz (40);
+        }
+    }
+
+    URL getLastDroppedFile() const noexcept { return lastFileDropped; }
+
+    void setZoomFactor (double amount)
+    {
+        if (thumbnail.getTotalLength() > 0)
+        {
+            auto newScale = jmax (0.001, thumbnail.getTotalLength() * (1.0 - jlimit (0.0, 0.99, amount)));
+            auto timeAtCentre = xToTime (getWidth() / 2.0f);
+
+            setRange ({ timeAtCentre - newScale * 0.5, timeAtCentre + newScale * 0.5 });
+        }
+    }
+
+    void setRange (Range<double> newRange)
+    {
+        visibleRange = newRange;
+        scrollbar.setCurrentRange (visibleRange);
+        updateCursorPosition();
+        repaint();
+    }
+
+    void setFollowsTransport (bool shouldFollow)
+    {
+        isFollowingTransport = shouldFollow;
     }
 
     void paint (Graphics& g) override
     {
-        /* This demo code just fills the component's background and
-           draws some placeholder text to get you started.
+        g.fillAll (Colours::darkgrey);
+        g.setColour (Colours::lightblue);
 
-           You should replace everything in this method with your own
-           drawing code..
-        */
+        if (thumbnail.getTotalLength() > 0.0)
+        {
+            auto thumbArea = getLocalBounds();
 
-        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));   // clear the background
-
-        g.setColour (Colours::grey);
-        g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
-
-        g.setColour (Colours::white);
-        g.setFont (14.0f);
-        g.drawText ("SamplerThumbnail", getLocalBounds(),
-                    Justification::centred, true);   // draw some placeholder text
+            thumbArea.removeFromBottom (scrollbar.getHeight() + 4);
+            thumbnail.drawChannels (g, thumbArea.reduced (2),
+                                    visibleRange.getStart(), visibleRange.getEnd(), 1.0f);
+        } else
+        {
+            g.setFont (14.0f);
+            g.drawFittedText ("(No audio file selected)", getLocalBounds(), Justification::centred, 2);
+        }
     }
 
     void resized() override
     {
-        // This method is where you should set the bounds of any child
-        // components that your component contains..
+        scrollbar.setBounds (getLocalBounds().removeFromBottom (14).reduced (2));
+    }
 
+    void changeListenerCallback (ChangeBroadcaster*) override
+    {
+        // this method is called by the thumbnail when it has changed, so we should repaint it..
+        repaint();
+    }
+
+    void mouseDown (const MouseEvent& e) override
+    {
+        mouseDrag (e);
+    }
+
+    void mouseDrag (const MouseEvent& e) override
+    {
+        if (canMoveTransport())
+            transportSource.setPosition (jmax (0.0, xToTime ((float) e.x)));
+    }
+
+    void mouseUp (const MouseEvent&) override
+    {
+        transportSource.start();
+    }
+
+    void mouseWheelMove (const MouseEvent&, const MouseWheelDetails& wheel) override
+    {
+        if (thumbnail.getTotalLength() > 0.0)
+        {
+            auto newStart = visibleRange.getStart() - wheel.deltaX * (visibleRange.getLength()) / 10.0;
+            newStart = jlimit (0.0, jmax (0.0, thumbnail.getTotalLength() - (visibleRange.getLength())), newStart);
+
+            if (canMoveTransport())
+                setRange ({ newStart, newStart + visibleRange.getLength() });
+
+            if (wheel.deltaY != 0.0f)
+                zoomSlider.setValue (zoomSlider.getValue() - wheel.deltaY);
+
+            repaint();
+        }
     }
 
 private:
+    AudioTransportSource& transportSource;
+    Slider& zoomSlider;
+    ScrollBar scrollbar { false };
+
+    AudioThumbnailCache thumbnailCache { 5 };
+    AudioThumbnail thumbnail;
+    Range<double> visibleRange;
+    bool isFollowingTransport = false;
+
+    DrawableRectangle currentPositionMarker;
+
+    URL lastFileDropped;    //TODO
+
+
+    float timeToX (const double time) const
+    {
+        if (visibleRange.getLength() <= 0)
+            return 0;
+
+        return getWidth() * (float) ((time - visibleRange.getStart()) / visibleRange.getLength());
+    }
+
+    double xToTime (const float x) const
+    {
+        return (x / getWidth()) * (visibleRange.getLength()) + visibleRange.getStart();
+    }
+
+    bool canMoveTransport() const noexcept
+    {
+        return !(isFollowingTransport && transportSource.isPlaying());
+    }
+
+
+    void scrollBarMoved (ScrollBar* scrollBarThatHasMoved, double newRangeStart) override
+    {
+        if (scrollBarThatHasMoved == &scrollbar)
+            if (!(isFollowingTransport && transportSource.isPlaying()))
+                setRange (visibleRange.movedToStartAt (newRangeStart));
+    }
+
+    void timerCallback() override
+    {
+        if (canMoveTransport())
+            updateCursorPosition();
+        else
+            setRange (visibleRange.movedToStartAt (transportSource.getCurrentPosition() - (visibleRange.getLength() / 2.0)));
+    }
+
+    void updateCursorPosition()
+    {
+        currentPositionMarker.setVisible (transportSource.isPlaying() || isMouseButtonDown());
+
+        currentPositionMarker.setRectangle (Rectangle<float> (timeToX (transportSource.getCurrentPosition()) - 0.75f, 0,
+                                            1.5f, (float) (getHeight() - scrollbar.getHeight())));
+    }
+
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SamplerThumbnail)
 };

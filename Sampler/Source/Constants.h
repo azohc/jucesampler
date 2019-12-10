15843e4194eb4a7184712d2db2c4d9b1d90e76fa
azohc <juanchozass@gmail.com>
2019-12-10 11:43:45 +0100

    constants file

---
+++ Sampler/Source/Constants.h
@@ -0,0 +1,42 @@
+/*
+  ==============================================================================
+
+    Constants.h
+    Created: 9 Dec 2019 7:50:56pm
+    Author:  chosa
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+
+// COLORS
+static const Colour COLOR_BGDARK = Colour::fromString("FF252420");
+static const Colour COLOR_BG =  Colour::fromString("FF544F4C");
+static const Colour COLOR_BGLIGHT = Colour::fromString("FFA1988F");
+static const Colour COLOR_FG =  Colour::fromString("FFEADED2");
+static const Colour COLOR_GRAY = Colour::fromString("FF605B58");
+static const Colour COLOR_GRAYLIGHT = Colour::fromString("FFD3D4D9");
+static const Colour COLOR_RED = Colour::fromString("FFD62734");
+static const Colour COLOR_BLUEDARK = Colour::fromString("FF252D39");
+static const Colour COLOR_BLUE = Colour::fromString("FF525C65");
+static const Colour COLOR_TAN = Colour::fromString("FFEDB183");
+static const Colour COLOR_SEA = Colour::fromString("FF1E555C");
+
+
+static const Identifier ID_CHOPS = "Chops";
+static const Identifier ID_CHOP = "Chop";
+static const Identifier PROPERTY_ID = "id";
+static const Identifier PROP_START_TIME = "StartTime";
+static const Identifier PROP_END_TIME = "EndTime";
+static const Identifier PROP_TRIGGER = "TriggerNote";
+static const Identifier PROP_HIDDEN = "Hidden";
+static const Identifier PROP_RECT_START = "StartMarker";
+static const Identifier PROP_RECT_END = "EndMarker";
+
+
+static const int NONE = -110;
+
